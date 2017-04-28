3e64a0919591ede3d137423d90f24240495240a5
mosson <cucation@gmail.com>
2017-04-28 16:12:40 +0900

    fix path

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,11 @@ import * as ReactDOM from 'react-dom';
 import App from './components/app';
 
 const srces = [
-  '/1.gif',
-  '/2.gif',
-  '/3.gif',
-  '/4.gif',
-  '/5.gif'
+  './1.gif',
+  './2.gif',
+  './3.gif',
+  './4.gif',
+  './5.gif'
 ];
 
 ReactDOM.render(<App srces={srces}/>, document.getElementById('app'));
